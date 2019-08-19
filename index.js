26cc500c29515f8392969cf2bb82a836533d33d7
Patrick <patrick.gunsolley@outlook.com>
2019-08-19 15:59:24 -0400

    quitting coffee not so good'

--- index.js
+++ index.js
@@ -212,7 +212,7 @@ SOFTWARE.
           }
           $timeout(function() {
             attrs[attribute].split('|').forEach(function(val) {
-              switch (attrs[attribute]) {
+              switch (val) {
                 case 'columnHeads': 
                   mtMutableTable.lockColumn(val);
                   break;
